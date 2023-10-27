2a6dcde1844d7e8bcfa84f4d20dcb49a0102d788
Aramis Martinez <martinezcruzaj@gmail.com>
2023-10-26 20:56:07 -0400

    Adding

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,26 +1,34 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * more headers goes there
+ * main - Checking if the number is positive or negative
+ * 
+ * Return: 0 always
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
+    /* This check if it less than 0 */
     if (n < 0)
     {
+        /* It prints the var n with 'is negative' and a newline */
         printf("%d is negative\n", n);
     }
+    /* This checks if it zero */
     else if (n == 0)
     {
+        /* It print the var n with 'is zero' and a newline */
         printf("%d is zero\n", n);
     }
+    /* Then if it doesn't pass the 2 first count then does this */
     else
     {
+        /* It print the var n with 'is positive' and a newline */
         printf("%d is positive\n", n);
     }
 	return (0);
--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -14,17 +14,23 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	d = n % 10;
+	/* It's check if the last digit is larger than 5 */
 	if (d > 5)
 	{
+		/* Then it print the digit and the last digit */
 		printf("Last digit of %i is %i and is greater than 5\n", n, d);
 	}
+	/* It's check if the last digit is equal to 0 */
 	else if (d == 0)
 	{
+		/* Then it print the digit and the last digit */
 		printf("Last digit of %i is %i and is 0\n", n, d);
 	}
+	/* It's does this if it doesn't pass the first 2 checks */
 	else
 	{
+		/* Then it print the digit and the last digit */
 		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, d);
-	};
+	}
 	return (0);
 }
