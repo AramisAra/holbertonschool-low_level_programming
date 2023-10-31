1d4f8e50b2ff760aff7c33182254736efbde3d8a
Aramis Martinez <martinezcruzaj@gmail.com>
2023-10-31 15:03:17 -0400

    adding bonus task

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - It adds 2 numbers together
+ * @a: first number
+ * @b:second number
+ * 
+ * Return: Added numbers
+*/
+int add(int a, int b)
+{
+    return (a + b);
+}
