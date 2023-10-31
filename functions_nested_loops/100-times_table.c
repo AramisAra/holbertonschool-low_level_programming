d8505bc3f9f972bfcc5da269efe0eafb7d199a11
Aramis Martinez <martinezcruzaj@gmail.com>
2023-10-31 16:41:56 -0400

    adding bonus task

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -18,15 +18,16 @@ void print_times_table(int n)
 				_putchar('0' + rep);
 			else if (rep < 10)
 			{
+                _putchar(' ');
+                _putchar(' ');
 				_putchar('0' + rep);
-                _putchar(' ');
-                _putchar(' ');
+
 			}
 			else if (rep < 100)
 			{   
+                _putchar(' ');
 				_putchar('0' + rep / 10);
 				_putchar('0' + rep % 10);
-                _putchar(' ');
 			}
 			else
 			{
