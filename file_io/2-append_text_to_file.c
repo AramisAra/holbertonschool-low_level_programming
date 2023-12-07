9a22952ba5b38fa61a72abd882ee2b4fe1de1c61
Aramis Martinez <martinezcruzaj@gmail.com>
2023-12-07 15:48:05 -0400

    Adding fixes

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -4,22 +4,22 @@
  * @filename: Name for the file
  * @text_content: The content of the file
  * Return: 1 on success, -1 on failure
-*/
+ */
 int append_text_to_file(const char *filename, char *textcontent)
 {
-            int fd;
+	int fd;
 
-        if (!filename)
-            return (-1);
+	if (!filename)
+		return (-1);
 
-        
-        if (access(filename, F_OK) == 0)
-            fd = open(filename, O_WRONLY | O_APPEND);
-        if (!textcontent)
-            return (1);
-        if (write(fd, textcontent, strlen(textcontent)) == -1)
-            return (-1);
-        close(fd);
 
-        return (1);
+	if (access(filename, F_OK) == 0)
+		fd = open(filename, O_WRONLY | O_APPEND);
+	if (!textcontent)
+		return (1);
+	if (write(fd, textcontent, strlen(textcontent)) == -1)
+		return (-1);
+	close(fd);
+
+	return (1);
 }
