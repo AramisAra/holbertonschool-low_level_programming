b59846dc176ecb003b2b1bb11e7c631b1f21e269
Aramis Martinez <martinezcruzaj@gmail.com>
2023-12-07 14:41:04 -0400

    Work for question 1 added

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,8 +1,4 @@
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
 #include "main.h"
-#include <stdio.h>
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
  * @filename: Name for the file to read
@@ -11,25 +7,26 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int rd, fd;
-    char *buf;
+    int wr,rd, fd;
+    char *buf = malloc(sizeof(char *) * letters);;
 
 
     if (filename == NULL)
         return (0);
-
-    buf = malloc(sizeof(char *) * letters);
     if (buf == NULL)
         return (0);
-
     fd = open(filename, O_RDONLY, 0600); 
     if (fd == -1)
         return (0);
     rd = read(fd, buf, letters);
-    write(STDOUT_FILENO, buf, rd);
+    if (rd == -1)
+        return (0);
+    wr = write(STDOUT_FILENO, buf, rd);
+    if (wr == -1)
+        return (0);
     
     free(buf);
     close(fd);
 
-    return (rd);
+    return (wr);
 }
--- file_io/main.h
+++ file_io/main.h
@@ -1,7 +1,17 @@
 #ifndef MAIN_h
 #define MAIN_h
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stddef.h>
+
 
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
