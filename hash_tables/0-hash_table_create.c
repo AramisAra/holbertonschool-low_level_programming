30052d0b37c07c495fd99492be4b69c45a03b375
Aramis Martinez <martinezcruzaj@gmail.com>
2023-12-08 09:24:13 -0400

    Update 0-hash_table_create.c

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -14,6 +14,7 @@ hash_table_t *hash_table_create(unsigned long int size)
         return (NULL);
     hash_table->size = size;
     array = malloc(sizeof(hash_node_t *) * size);
+    hash_table->array = array;
     if (array == NULL)
         return (NULL);
     return (hash_table);
