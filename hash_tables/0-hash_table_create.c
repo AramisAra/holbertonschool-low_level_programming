b1bcbe7e7712e2f3699c92360c97351f391a802c
Aramis Martinez <martinezcruzaj@gmail.com>
2023-12-08 09:35:58 -0400

    Fixing betty problems

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -3,20 +3,20 @@
  * hash_table_create - creates a hash table
  * @size: size of the array
  * Return: poinyer to hash table
-*/
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *hash_table;
-    hash_node_t **array;
+	hash_table_t *hash_table;
+	hash_node_t **array;
 
-    hash_table = malloc(sizeof(hash_table_t));
-    if (hash_table == NULL)
-        return (NULL);
-    hash_table->size = size;
-    array = malloc(sizeof(hash_node_t *) * size);
-    hash_table->array = array;
-    if (array == NULL)
-        return (NULL);
-    return (hash_table);
-    
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
+		return (NULL);
+	hash_table->size = size;
+	array = malloc(sizeof(hash_node_t *) * size);
+	hash_table->array = array;
+	if (array == NULL)
+		return (NULL);
+	return (hash_table);
+
 }
