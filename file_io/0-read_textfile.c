fbba095f239bb9d273396d38c3a0a18fbeb41be7
Aramis Martinez <martinezcruzaj@gmail.com>
2023-12-08 12:43:25 -0400

    Adding fixes for task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -7,14 +7,14 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int wr,rd, fd;
+	int wr, rd, fd;
 	char *buf = malloc(sizeof(char *) * letters);
 
 	if (filename == NULL)
 		return (0);
 	if (buf == NULL)
 		return (0);
-	fd = open(filename, O_RDONLY, 0600); 
+	fd = open(filename, O_RDONLY, 0600);
 	if (fd == -1)
 		return (0);
 	rd = read(fd, buf, letters);
